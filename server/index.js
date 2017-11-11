2228ebca2376d8f9aab62dd833160db1286cf14f
Chayakorn Chayaphaniphatt <58160285@go.buu.ac.th>
2017-11-11 15:30:22 +0700

    students

--- server/index.js
+++ server/index.js
@@ -7,18 +7,27 @@ app.get('/greeting', (req, res) => {
         th: 'สวัสดี'
     }
 
-
     let l = req.query.lang
-    if(!l){
-        res.json({ message: 'Hello'})
-    }else{
-        res.json({ message: lang[l]})
+    if (!l) {
+        res.json({ message: 'Hello' })
+    } else {
+        res.json({ message: lang[l] })
     }
 
-    
+})
+   let students = [
+{
+    id: 1 , name: 'Chayakorn' , u: 'buu' , year: 2001 
+} ,
 
+{
+    id: 2 , name: 'Fill' , u: 'tu' , year: 1999 
+}
 
-
+    ]
+app.get('/students', (req, res) => {
+ 
+res.json(students)
 })
 
 
